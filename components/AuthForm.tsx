010e24ec33a12f053ee69a56d224c77f69e85c8f
snehasingh022 <snehasingh842003@gmail.com>
2025-06-12 22:58:34 +0530

    setup app, file and folder structure routing and authpages

---
+++ components/AuthForm.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+import { z } from "zod";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+const authFormSchema = (type: FormType) => {
+  return z.object({
+    name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(3),
+  });
+};
+
+function AuthForm({ type }: { type: FormType }) {
+  const formSchema = authFormSchema(type);
+  const router=useRouter()
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      if (type === "sign-up") {
+        toast.success('Account created sucessfully.Please sign in');
+        router.push('/sign-in')
+        console.log("SIGN UP", values);
+      } else {
+        toast.success(' sign in sucessfully');
+        router.push('/')
+        console.log("SIGN IN", values);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(`There was an error ${error}`);
+    }
+  }
+
+  const isSignIn = type === "sign-in";
+
+  return (
+    <div className="card-border lg:min-w-[566px]">
+      <div className="flex flex-col gap-6 card py-14 px-10">
+        <div className="flex flex-row gap-2 justify-center">
+          <Image src="/logo.svg" alt="logo" height={32} width={38} />
+          <h2 className="text-primary-100">PrepWise</h2>
+        </div>
+        <h3>Practice job interview with AI</h3>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+            {!isSignIn && (
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Your Name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Your Email Address" type="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your password" type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button className="btn" type="submit">
+              {isSignIn ? "Sign In" : "Create An Account"}
+            </Button>
+          </form>
+        </Form>
+
+        <p className="text-center">
+          {isSignIn ? "No account yet?" : "Have an account already?"}
+          <Link href={!isSignIn ? "/sign-in" : "/sign-up"} className="font-bold text-user-primary ml-1">
+            {!isSignIn ? "Sign In" : "Sign Up"}
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AuthForm;
---
+++ components/FormField.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Controller,FieldValues,Path,Control } from 'react-hook-form';
+interface FormFieldProps<T extends FieldValues>{
+    control:Control<T>;
+    name:Path<T>;
+    label:string;
+    placeholder?:string;
+    type:'text'|'email'|'password'|'file'
+}
+
+const FormFieldComponent = ({control,name,label,placeholder,type='text'}:FormFieldProps<T>) => (
+ <Controller
+ control={control}  
+ name={name} 
+ render={({field})=>(
+<FormItem>
+        <FormLabel className='label'>{label}</FormLabel>
+        <FormControl>
+          <Input className="input" placeholder={placeholder} {...field} />
+        </FormControl>
+        
+        <FormMessage />
+      </FormItem>
+)}
+      
+    
+  />
+);
+
+export default FormFieldComponent;
